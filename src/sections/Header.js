ead1709b9dfaeb830ef3100a87327dda3fb73579
Luka Kilaberidze <lkila16@freeuni.edu.ge>
2022-02-22 22:54:28 +0400

    testing staging changes

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -5,7 +5,7 @@ function Header() {
     <header>
       <div class="header-wrapper">
         <h1 class="header">SHA-შაკოლა</h1>
-        <h3 class="subheader">პირველი ქართული Hash ფუნქცია</h3>
+        <h3 class="subheader">პირველი და 100% ქართული Hash ფუნქცია</h3>
       </div>
     </header>
   );
